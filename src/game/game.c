fb2121f13f507dfc7dd7c9f68e1b05845048d248
MORIN Maxence <astraxalumix@gmail.com>
2019-10-18 14:53:17 +0200

    change the color system

--- src/game/game.c
+++ src/game/game.c
@@ -11,7 +11,7 @@ void main_loop(WINDOW *left_board, WINDOW *right_board) {
 	new_board(ia_board);
 
 	place_ia_boats(ia_board, &ia);
-	print_board_without_boat(ia_board, left_board, "IA board");
+	print_board_with_boat(ia_board, left_board, "IA board");
 	
 	ask_player_to_place_boats(right_board, human_board, &human);
 	print_board_with_boat(human_board, right_board, "Player board");
